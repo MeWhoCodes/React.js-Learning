6add75b199f3eca11f5d6ec10dc25cccf0e503db
Devansh Sharma <devaton4@gmail.com>
2025-12-30 11:44:56 +0530

    mini project

---
+++ 00_mini/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [pos, setPos] = useState({ x: 100, y: 100 });
+  const [dragging, setDragging] = useState(false);
+  const [siz, setSize] = useState(100);
+
+  const handleMouseDown = () => {
+    setDragging(true);
+  };
+
+  const handleMouseUp = () => {
+    setDragging(false);
+  };
+
+  const handleMouseMove = (e) => {
+    if (!dragging) return;
+
+    setPos({
+      x: e.clientX - siz/2,
+      y: e.clientY - siz/2,
+    });
+  };
+
+  return (
+    <div
+      className="container"
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+    >
+      <div
+        className={`circle ${dragging ? "dragging" : ""}`}
+        onMouseDown={handleMouseDown}
+        style={{
+          width: `${siz}px`,
+          height: `${siz}px`,
+          left: pos.x,
+          top: pos.y,
+        }}
+      />
+
+      <input type="range" name="Size" value={siz} min={10} max={1000} onChange={(e)=>{setSize(e.target.value)}}/>
+      <div>Size: {siz}</div>
+    </div>
+  );
+}
